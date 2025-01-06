b88826ed8a3be9351d489fe5d75103dc2f830d3f
Abhay <robertsteeve789@gmail.com>
2025-01-06 13:33:36 +0530

    I ntegrate some functionality

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
 import { Stack } from "expo-router";
 import React from 'react';
 import StackRoute from './index';
+import { BasketContext} from '../Context';
 
 export default function RootLayout() {
   return (
+    <BasketContext>
     <Stack screenOptions={{ headerShown: false }}>
       <StackRoute />
     </Stack>
+    </BasketContext>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,58 +1,53 @@
+// import {View, Text} from 'react-native';
 // import React from 'react';
-// import {Login } from '../src/screens/Login/Login';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-// // import {BasicDetails} from '../src/screens/BasicDetails';
-// // import {BusinessTypeSelection} from '../src/screens/BusinessTypeSelection';
+// import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// import Home from '../src/screens/Home/Home';
+// // import Splash from '../screens/Splash/Splash';
+// import {Login} from '../src/screens/Login/Login';
+// // import Cart from '../screens/Cart/Cart';
+// // import Details from '../screens/Details/Details';
+// // import Wishlist from '../screens/Wishlist/Wishlist';
 
-// // import { HomeScreen } from '../src/screens/HomeScreen';
-// // import  LanguageSelection from '../src/screens/LanguageSelection'
 // const Stack = createNativeStackNavigator();
-// const App = () => {
-  
+// const StackRoute = () => {
 //   return (
-    
-//     <NavigationContainer independent={true}>
-//       {/* <Stack.Navigator initialRouteName="Login"> */}
-//         <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-//         {/* <Stack.Screen name="BusinessTypeSelection" component={BusinessTypeSelection} options={{ headerShown: false }}/> */}
-//         {/* <Stack.Screen name="BasicDetails" component={BasicDetails} options={{ headerShown: false }} /> */}
-
-//       {/* </Stack.Navigator> */}
-//     </NavigationContainer>
+//     <Stack.Navigator
+//       screenOptions={{headerShown: false}}
+//       initialRouteName="Home">
+//       {/* <Stack.Screen name="Home" component={Home} />
+//       <Stack.Screen name="Splash" component={Splash} /> */}
+//       <Stack.Screen name="Home" component={Home} />
+//       <Stack.Screen name="Login" component={Login} />
+//       {/* <Stack.Screen name="Cart" component={Cart} />
+//       <Stack.Screen name="Details" component={Details} />
+//       <Stack.Screen name="Wishlist" component={Wishlist} /> */}
+//     </Stack.Navigator>
 //   );
-  
-// }
-// export default App;
+// };
+
+// export default StackRoute;
 
 
-import {View, Text} from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../src/screens/HomeScreen";
+import HotelRoom from "../src/screens/HotelRoom";
+import OrderData from "../src/screens/OrderData";
 
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Home from '../src/screens/Home/Home';
-// import Splash from '../screens/Splash/Splash';
-import {Login} from '../src/screens/Login/Login';
-// import Cart from '../screens/Cart/Cart';
-// import Details from '../screens/Details/Details';
-// import Wishlist from '../screens/Wishlist/Wishlist';
-
-const Stack = createNativeStackNavigator();
 const StackRoute = () => {
+    const Stack = createNativeStackNavigator();
   return (
-    <Stack.Navigator
-      screenOptions={{headerShown: false}}
-      initialRouteName="Home">
-      {/* <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Splash" component={Splash} /> */}
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Login" component={Login} />
-      {/* <Stack.Screen name="Cart" component={Cart} />
-      <Stack.Screen name="Details" component={Details} />
-      <Stack.Screen name="Wishlist" component={Wishlist} /> */}
-    </Stack.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown:false}}/>
+        <Stack.Screen name="HotelRoom" component={HotelRoom} options={{headerShown:false}}/>
+        <Stack.Screen name="OrderData" component={OrderData} options={{headerShown:false}}/>
+      </Stack.Navigator>
   );
 };
 
 export default StackRoute;
+
+const styles = StyleSheet.create({});
